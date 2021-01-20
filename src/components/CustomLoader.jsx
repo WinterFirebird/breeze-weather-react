dbb2f7eee40b8eb067eda06499ff1f092203c6f5
WinterFirebird <naprimerarman@gmail.com>
2021-01-20 13:28:33 +0400

    loader design is now persistent across different platforms

--- src/components/CustomLoader.jsx
+++ src/components/CustomLoader.jsx
@@ -1,6 +1,32 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const LoadingGrid = styled.div`
+  border: 1px solid #000;
+  display: grid;
+  grid-template-columns: 2fr 3fr 1fr;
+  > div {
+    width: 100%;
+    height: 10px;
+    background-color: #000;
+  }
+`;
+
+const SegmentStyle = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100vw',
+  height: '100vh',
+  margin: 0,
+}
+
+const SegmentHeaderStyle = {
+  whiteSpace: 'nowrap',
+  color: '#000',
+}
 
 class CustomLoader extends Component {
   render() {
@@ -10,28 +36,24 @@ class CustomLoader extends Component {
       isReverseGeocodingResponseReady,
     } = this.props;
     return (
-      <Segment style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh' }}>
+      <Segment style={SegmentStyle}>
         <Dimmer active inverted>
           {isLocationResponseReady ? (
             <Loader content inverted>
-              <h1>Please wait...</h1>
+              <h1 style={SegmentHeaderStyle}>Please wait...</h1>
               <h3>Fetching data</h3>
-              <progress
-                value={(isWeatherResponseReady || isReverseGeocodingResponseReady) ? ((isWeatherResponseReady && isReverseGeocodingResponseReady) ? '100' : '80') : '30'} 
-                max="100"
-              >
-              </progress>
-              {isWeatherResponseReady ? <div></div> : null}
+              <LoadingGrid>
+                <div></div>
+                {isWeatherResponseReady? <div></div> : null}
+                {isReverseGeocodingResponseReady? <div></div> : null}
+              </LoadingGrid>
               <h4>Stuck? It might be caused by an AdBlocker</h4>
             </Loader>
             ) : (
             <Loader indeterminate inverted>
-              <h1>Please wait...</h1>
+              <h1 style={SegmentHeaderStyle}>Please wait...</h1>
               <h3>Fetching data</h3>
-              <progress 
-              value="0" max="100"
-              >
-              </progress>
+              <LoadingGrid />
               <h4>Stuck? It might be caused by an AdBlocker</h4>
             </Loader>
             )}
